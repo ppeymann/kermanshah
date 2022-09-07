ccf77f5ee9b74fe26ef3663f31709ae0c6ca36ef
peyman malek <p.malek32@gmail.com>
2022-09-08 02:09:47 +0430

    Navbar is compelet

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 // icon import
 import {BiSearch} from "react-icons/bi"
 import {BsPerson} from "react-icons/bs"
 import {HiOutlineMenu} from "react-icons/hi"
+import {FaFacebook ,FaInstagram ,FaTwitter ,FaPinterest ,FaYoutube} from "react-icons/fa"
 // css import
 import "./NavbarStyle.css"
 
 const Navbar = () => {
+
+    const [activate , setActivate] = useState(false)
+    const handleNav = ()=>setActivate(!activate)
+
   return (
-    <div className='navbar'>
+    <div className={activate ? "navbar navbar-bg":"navbar"}>
         <div className='logo'>
             <h2>کرمانشاه</h2>
         </div>
@@ -20,11 +25,33 @@ const Navbar = () => {
             <li>تماشا</li>
         </ul>
         <div className='nav-icons'>
-            <BiSearch className='icon' />
+            <BiSearch className='icon' style={{marginLeft:"1rem"}} />
             <BsPerson className="icon" />
         </div>
-        <div className='hamburger'>
+        <div className='hamburger' onClick={handleNav}>
             <HiOutlineMenu className="icon" />
+        </div>
+        <div className={activate? "mobile-menu active":"mobile-menu"}>
+            <ul className='mobile-nav'>
+              <li>خانه</li>
+              <li>مقصد</li>
+              <li>مسافرت</li>
+              <li>رزرو</li>
+              <li>تماشا</li>
+            </ul>
+            <div className='mobile-menu-bottom'>
+                <div className='menu-icon'>
+                    <button className=''>پیدا کن</button>
+                    <button className=''>ورود</button>
+                </div>
+                <div className='social-icon'>
+                    <FaFacebook className="icon" />
+                    <FaInstagram className="icon" />
+                    <FaTwitter className="icon" />
+                    <FaPinterest className="icon" />
+                    <FaYoutube className="icon" />
+                </div>
+            </div>
         </div>
     </div>
   )
